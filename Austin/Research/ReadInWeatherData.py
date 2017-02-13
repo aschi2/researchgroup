44acd827b9b7d78441dfac8ce764732bd26e8652
aschi2 <aschi@ucdavis.edu>
2017-02-13 10:30:44 -0800

    Minor Fixes

--- Austin/Research/ReadInWeatherData.py
+++ Austin/Research/ReadInWeatherData.py
@@ -3,7 +3,7 @@ import json
 import pandas
 
 def DSTimeMachine(lat, lon, time,api = 'ae81c74d7b434000492e19b44facafc0'):
-    """Takes lat (float), lon (float), time (int) and api (string) (optional), returns an 1 x n pandas dataframe."""
+    """Takes lat (float), lon (float), time (int) and api key (string) (optional), returns an 1 x n pandas dataframe."""
     url = "https://api.darksky.net/forecast/%s/%f,%f,%d"
     query = url % (api,lat,lon,time)
     r = requests.get(query)
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Research Group
 
 # Functions
 ##	DSTimeMachine(lat,lon, time,api)
-Takes lat (float), lon (float), time (int) and api (string) (optional), returns an 1 x n pandas dataframe.
+Takes lat (float), lon (float), time (int) and api key (string) (optional), returns an 1 x n pandas dataframe.
 
 
 
