ddb26303ba5829edd1038a2ba7e830c7a5c63d87
aschi2 <aschi@ucdavis.edu>
2017-02-14 00:25:05 -0800

    Quick Commit

--- Austin/Research/FeatureScraper.py
+++ Austin/Research/FeatureScraper.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import pandas
+from datetime import datetime
+from urllib2 import Request, urlopen
 
 def DSTimeMachine(lat, lon, time,api = 'ae81c74d7b434000492e19b44facafc0'):
     """Takes lat (float), lon (float), time (int) and api key (string) (optional), returns an 1 x n pandas dataframe."""
@@ -12,19 +14,27 @@ def DSTimeMachine(lat, lon, time,api = 'ae81c74d7b434000492e19b44facafc0'):
     test2 = pandas.DataFrame.transpose(testerdf)
     return test2
 
+def toUnix(d, time):
+    """Takes in Date (DateTime Object?) and Time(Int?), returns unix time"""
+    time = str(time)
+    if (len(time) == 3):
+        time = '0' + time
+    extra = time[len(time) - 2:len(time)]
+    extra = ":" + extra
+    time = time[:-2]
+    time = time + extra
+    date = d.strftime('%Y-%m-%d')
+    date = date+" "+time
+    datetime_object = datetime.strptime(date, '%Y-%m-%d %H:%M')
+    return (datetime_object - datetime(1970,1,1)).total_seconds()
 
-
-def ElAPI(lat,lon,api = "AIzaSyCDrKYZg1yY9uGU078F-vgtw9T2mcmn4-0"):
+def GOElevation(lat,lon,api = "AIzaSyCDrKYZg1yY9uGU078F-vgtw9T2mcmn4-0"):
     """Takes lat (float), lon (float), and api key (string) (optional), returns a float number """
     url = "https://maps.googleapis.com/maps/api/elevation/json?locations=%f,%f&key=%s"
     qurl = url % (lat,lon,api)
     r = requests.get(qurl)
     df = pandas.read_json(r.text)
     dfprime = pandas.DataFrame(df)
-    flt = dfprime["results"][0]['elevation']
+    flt = [dfprime["results"][0]['elevation']]
     dfprime2 = pandas.DataFrame(flt,columns=["Elevation"])
     return dfprime2
-
-
-
-    
--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 Research Group
 
 # Functions
-##	DSTimeMachine(lat,lon, time,api)
-Takes lat (float), lon (float), time (int) and api key (string) (optional), returns an 1 x n pandas dataframe.
-
+##	DSTimeMachine(lat,lon,time,api)
+Takes lat (float), lon (float), time (int), and api key (string) (optional), returns an 1 x n pandas dataframe.
+## toUnix(d,time)
+Takes in Date (DateTime Object?) and Time(Int?), returns unix time.
+## GOElevation(lat,lon,api)
+"""Takes lat (float), lon (float), and api key (string) (optional), returns a float number """
 
 
 
