67d7fbf838b0d72fb1099b82d8b306371f07e46c
aschi2 <austinchi2@yahoo.com>
2017-04-09 16:34:27 -0700

    Quick Commit

--- Austin/Research/FeatureScraper.py
+++ Austin/Research/FeatureScraper.py
@@ -3,6 +3,7 @@ import json
 import pandas
 from datetime import datetime
 from urllib2 import Request, urlopen
+import numpy as np
 
 def DSTimeMachine(lat, lon, time,api = 'ae81c74d7b434000492e19b44facafc0'):
     """Takes lat (float), lon (float), time (int) and api key (string) (optional), returns an 1 x n pandas dataframe."""
@@ -10,9 +11,12 @@ def DSTimeMachine(lat, lon, time,api = 'ae81c74d7b434000492e19b44facafc0'):
     query = url % (api,lat,lon,time)
     r = requests.get(query)
     df = pandas.read_json(r.text)
-    testerdf = pandas.DataFrame(df['currently'])
-    test2 = pandas.DataFrame.transpose(testerdf)
-    return test2
+    try:
+        testerdf = pandas.DataFrame(df['currently'])
+        test2 = pandas.DataFrame.transpose(testerdf)
+        return test2
+    except:
+        return np.nan
 
 def toUnix(d, time):
     """Takes in Date (DateTime Object?) and Time(Int?), returns unix time"""
@@ -35,24 +39,27 @@ def GOElevation(lat,lon,api = "AIzaSyCDrKYZg1yY9uGU078F-vgtw9T2mcmn4-0"):
     r = requests.get(qurl)
     df = pandas.read_json(r.text)
     dfprime = pandas.DataFrame(df)
-    flt = [dfprime["results"][0]['elevation']]
-    dfprime2 = pandas.DataFrame(flt,columns=["Elevation"])
-    return dfprime2
+    try:
+        flt = [dfprime["results"][0]['elevation']]
+        dfprime2 = pandas.DataFrame(flt,columns=["Elevation"])
+        return dfprime2
+    except:
+        return np.nan
 
-def ReverseGeo(latitude = 35.1330343,longitude = -90.0625056,api = 'AIzaSyBwTLTIHYJU_osZ-KKE-HlTH9EcowYJjDs'):  
+def ReverseGeo(lat=35.1330343, lon=-90.0625056, api='AIzaSyBwTLTIHYJU_osZ-KKE-HlTH9EcowYJjDs'):
     sensor = 'false'
-    # Hit Google's reverse geocoder directly
-    # NOTE: I *think* their terms state that you're supposed to
-    # use google maps if you use their api for anything.
     base = "https://maps.googleapis.com/maps/api/geocode/json?"
     params = "latlng={lat},{lon}&sensor={sen}&result_type=postal_code&key={api}".format(
-        lat=latitude,
-        lon=longitude,
+        lat=lat,
+        lon=lon,
         sen=sensor,
         api=api
     )
     url = "{base}{params}".format(base=base, params=params)
     response = requests.get(url)
     x = json.loads(response.text)
-    zipcode = x['results'][0]['address_components'][0]['short_name']
-    return zipcode
+    try:
+        zipcode = x['results'][0]['address_components'][0]['short_name']
+        return zipcode
+    except:
+        return np.nan
